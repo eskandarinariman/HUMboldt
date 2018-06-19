efebd005c63ad5165763f8696773e20c1a43db70
Nariman Eskandari <eskandarinariman@gmail.com>
2018-06-19 23:48:12 +0000

    generate hls has app bridge

--- generate_hls_ip.tcl
+++ generate_hls_ip.tcl
@@ -119,6 +119,21 @@ export_design -rtl verilog -format ip_catalog
 close_project
 
 
+
+open_project application_bridge
+set_top application_bridge
+add_files ../HMPI/HLS_lib/application_bridge.cpp
+#add_files -tb test_echo_server_application.cpp
+open_solution "solution1"
+set_part {xcku115-flva1517-2-e}
+create_clock -period 6.4 -name default
+config_interface -expose_global 
+#csim_design -clean -setup
+csynth_design
+export_design -format ip_catalog -display_name "Application Bridge" 
+
+
+
 # #userIP
 set MPI_HEADERS_DIR "./HLS_lib/"
 
